394cb944b62a8c6719a50d51e4a82dcc435c81c9
kareb <email@example.com>
2023-03-17 09:58:53 +0000

    adding changes

---
+++ components/navigation.php
@@ -0,0 +1,48 @@
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+        <a href="https://flowbite.com" class="flex items-center">
+            <img src="assets/images/logo.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+            <span class="self-center text-x2 font-semibold whitespace-nowrap dark:text-white">Careers</span>
+        </a>
+        <div class="flex items-center">
+            <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="<?= ROOT_DIR ?>faq" class="text-gray-900 dark:text-white hover:underline" aria-current="page">FAQ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<nav class="bg-gray-50 dark:bg-gray-700" style="background-color: #ffa400;">
+    <div class="max-w-screen-xl px-4 py-6 mx-auto md:px-6">
+        <div class="flex items-center">
+            <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+                <li>
+                    <a href="#" class="text-white dark:text-white hover:underline" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white dark:text-white hover:underline">Company</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white dark:text-white hover:underline">Team</a>
+                </li>
+                <li>
+                    <a href="#" class="text-white dark:text-white hover:underline">Features</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,243 @@
+<?php  
+include 'components/header.php';
+include 'components/navigation.php';
+// header 
+?>
+<header id="up" class="bg-center bg-fixed bg-no-repeat bg-center bg-cover h-screen relative" style="background-image: url(assets/images/header-index.jpg);">
+<!-- Overlay Background + Center Control -->
+<div class="h-screen bg-opacity-50 bg-black flex items-center justify-center">
+    <div class="mx-2 text-center">
+        <h1 class="text-gray-100 font-extrabold text-4xl xs:text-5xl md:text-6xl">
+            Work with us
+           </h1>
+           <div class="inline-flex">
+           <button class="p-2 my-5 mx-2 bg-indigo-700 hover:bg-indigo-800 font-bold text-white rounded border-2 border-transparent hover:border-indigo-800 shadow-md transition duration-500 md:text-xl">Vacancies</button>
+           <a href="#about"><button class="p-2 my-5 mx-2 bg-transparent border-2 bg-indigo-200 bg-opacity-75 hover:bg-opacity-100 border-indigo-700 rounded hover:border-indigo-800 font-bold text-indigo-800 shadow-md transition duration-500 md:text-lg">Benefits</button></a>
+           </div>
+        </div>
+    </div>
+</header>
+<!-- top section -->
+<div class="flex justify-center items-center">
+  <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ --->
+  <div class="2xl:mx-auto 2xl:container lg:px-20 lg:py-16 md:py-12 md:px-6 py-9 px-4 w-96 sm:w-auto">
+    <div role="main" class="flex flex-col items-center justify-center">
+      <h1 class="text-4xl font-semibold leading-9 text-center text-gray-800 dark:text-gray-50">This Week Blogs</h1>
+      <p class="text-base leading-normal text-center text-gray-600 dark:text-white mt-4 lg:w-1/2 md:w-10/12 w-11/12">If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough</p>
+    </div>
+    <div class="lg:flex items-stretch md:mt-12 mt-8">
+      <div class="lg:w-1/2">
+        <div class="sm:flex items-center justify-between xl:gap-x-8 gap-x-6">
+          <div class="sm:w-1/2 relative">
+            <div>
+              <p class="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">12 April 2021</p>
+              <div class="absolute bottom-0 left-0 p-6">
+                <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
+                <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
+                <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                  <p class="pr-2 text-sm font-medium leading-none">Read More</p>
+                  <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <img src="https://i.ibb.co/DYxtCJq/img-1.png" class="w-full" alt="chair" />
+          </div>
+          <div class="sm:w-1/2 sm:mt-0 mt-4 relative">
+            <div>
+              <p class="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">12 April 2021</p>
+              <div class="absolute bottom-0 left-0 p-6">
+                <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
+                <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
+                <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                  <p class="pr-2 text-sm font-medium leading-none">Read More</p>
+                  <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <img src="https://i.ibb.co/3C5HvxC/img-2.png" class="w-full" alt="wall design" />
+          </div>
+        </div>
+        <div class="relative">
+          <div>
+            <p class="md:p-10 p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">12 April 2021</p>
+            <div class="absolute bottom-0 left-0 md:p-10 p-6">
+              <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
+              <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
+              <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                <p class="pr-2 text-sm font-medium leading-none">Read More</p>
+                <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <img src="https://i.ibb.co/Ms4qyXp/img-3.png" alt="sitting place" class="w-full mt-8 md:mt-6 hidden sm:block" />
+          <img class="w-full mt-4 sm:hidden" src="https://i.ibb.co/6XYbN7f/Rectangle-29.png" alt="sitting place" />
+        </div>
+      </div>
+      <div class="lg:w-1/2 xl:ml-8 lg:ml-4 lg:mt-0 md:mt-6 mt-4 lg:flex flex-col justify-between">
+        <div class="relative">
+          <div>
+            <p class="md:p-10 p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">12 April 2021</p>
+            <div class="absolute bottom-0 left-0 md:p-10 p-6">
+              <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
+              <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
+              <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                <p class="pr-2 text-sm font-medium leading-none">Read More</p>
+                <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <img src="https://i.ibb.co/6Wfjf2w/img-4.png" alt="sitting place" class="w-full sm:block hidden" />
+          <img class="w-full sm:hidden" src="https://i.ibb.co/dpXStJk/Rectangle-29.png" alt="sitting place" />
+        </div>
+        <div class="sm:flex items-center justify-between xl:gap-x-8 gap-x-6 md:mt-6 mt-4">
+          <div class="relative w-full">
+            <div>
+              <p class="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">12 April 2021</p>
+              <div class="absolute bottom-0 left-0 p-6">
+                <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
+                <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
+                <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                  <p class="pr-2 text-sm font-medium leading-none">Read More</p>
+                  <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <img src="https://i.ibb.co/3yvZBpm/img-5.png" class="w-full" alt="chair" />
+          </div>
+          <div class="relative w-full sm:mt-0 mt-4">
+            <div>
+              <p class="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">12 April 2021</p>
+              <div class="absolute bottom-0 left-0 p-6">
+                <h2 class="text-xl font-semibold 5 text-white">The Decorated Ways</h2>
+                <p class="text-base leading-4 text-white mt-2">Dive into minimalism</p>
+                <a href="javascript:void(0)" class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
+                  <p class="pr-2 text-sm font-medium leading-none">Read More</p>
+                  <svg class="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <img src="https://i.ibb.co/gDdnJb5/img-6.png" class="w-full" alt="wall design" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- section -->
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-4">
+        <div class="p-4 md:w-1/3">
+            <div class="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+            <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
+            <div class="p-6">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
+                <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
+                <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                <div class="flex items-center flex-wrap ">
+                <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
+                
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="p-4 md:w-1/3">
+            <div class="h-full rounded-xl shadow-cla-violate bg-gradient-to-r from-pink-50 to-red-50 overflow-hidden">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
+                <div class="p-6">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
+                <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
+                <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                <div class="flex items-center flex-wrap ">
+                    <button class="bg-gradient-to-r from-orange-300 to-amber-400 hover:scale-105 drop-shadow-md shadow-cla-violate px-4 py-1 rounded-lg">Learn more</button>
+                    
+                </div>
+                </div>
+            </div>
+            </div>
+            <div class="p-4 md:w-1/3">
+            <div class="h-full rounded-xl shadow-cla-pink bg-gradient-to-r from-fuchsia-50 to-pink-50 overflow-hidden">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
+                <div class="p-6">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
+                <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
+                <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                <div class="flex items-center flex-wrap ">
+                    <button class="bg-gradient-to-r from-fuchsia-300 to-pink-400 hover:scale-105  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
+                    
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-4">
+        <div class="p-4 md:w-1/3">
+            <div class="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+            <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
+            <div class="p-6">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
+                <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
+                <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                <div class="flex items-center flex-wrap ">
+                <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
+                
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="p-4 md:w-1/3">
+            <div class="h-full rounded-xl shadow-cla-violate bg-gradient-to-r from-pink-50 to-red-50 overflow-hidden">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="assets/images/bg-jigsaw.jpg" alt="blog">
+                <div class="p-6">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
+                <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
+                <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                <div class="flex items-center flex-wrap ">
+                    <button class="bg-gradient-to-r from-orange-300 to-amber-400 hover:scale-105 drop-shadow-md shadow-cla-violate px-4 py-1 rounded-lg">Learn more</button>
+                    
+                </div>
+                </div>
+            </div>
+            </div>
+            <div class="p-4 md:w-1/3">
+            <div class="h-full rounded-xl shadow-cla-pink bg-gradient-to-r from-fuchsia-50 to-pink-50 overflow-hidden">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
+                <div class="p-6">
+                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
+                <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
+                <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                <div class="flex items-center flex-wrap ">
+                    <button class="bg-gradient-to-r from-fuchsia-300 to-pink-400 hover:scale-105  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
+                    
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- box img section -->
+
+<?php
+include 'components/footer.php';
+?>
+
+
+
+
